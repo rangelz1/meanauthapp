38eaddd5ae300ec6cb3f291b4c67047e9c2d7090
rangelz1 <rangelz187@gmail.com>
2017-04-29 13:16:34 -0700

    Routes and password secret authentication set up done

--- app.js
+++ app.js
@@ -4,6 +4,20 @@ const bodyParser = require('body-parser');
 const cors = require('cors');
 const passport = require('passport');
 const mongoose = require('mongoose');
+const config = require('./config/database');
+
+// Connect to Database
+mongoose.connect(config.database);
+
+// On Connection
+mongoose.connection.on('connected', () => {
+	console.log('Connected to database '+config.database);
+});
+
+// Error Message
+mongoose.connection.on('error', (err) => {
+	console.log('Database error '+err);
+});
 
 const app = express();
 
@@ -16,6 +30,9 @@ const port = 3000;
 
 // CORS Middleware
 app.use(cors());
+
+// Set Static Folder
+app.use(express.static(path.join(__dirname, 'public')));
 
 // Body Parser Middleware
 app.use(bodyParser.json());
